638489fcc6bbb26ed69bc63aeeda6d3cfedc00c2
smulrow <smulrow@gmail.com>
2022-06-22 22:30:26 -0700

    adding changes

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ var createBtn = document.getElementById("createBtn")
 var quizDiv = document.getElementById("quizDiv")
 var answer1 = document.getElementById("answer1")
 var timeEl = document.querySelector(".countdown")
-var mainEl =document.getElementById("main")
+var mainEl = document.getElementById("main")
 var counter = 60;
+var buttons = document.getElementById("buttons") 
+var finalScore = document.getElementById("finalScore")
+var saveScore = document.getElementById("saveScore")
+
+
 
 var questions = [
 {
@@ -37,44 +42,43 @@ function setTime() {
   
   var timerInterval = setInterval(function(){
     counter--;
-    timeEl.textContent = counter + "seconds remainig";
+    timeEl.textContent = counter + " seconds remainig";
 
     if (counter <= 0){
       clearInterval(timerInterval);
-      $("#countdown").html("<h3>Quiz Over</h3>");
+      quizOver();
       return;
-    }else{
-      $("#countdown").text(counter);
-      console.log("Countdown -->" + counter)
     }
   }, 1000);
+
+    
+  }
+
+
+  function incorrect(){
+    counter -=5;
+
   }
 
 
 
+function quizStart(){
+  question1()
+  }
 
-
-function quizStart() {
+function question1 () {
   quizH1.textContent = questions[0].question
   answer1.textContent = questions[0].choices[0]
   answer2.textContent = questions[0].choices[1]
   answer3.textContent = questions[0].choices[2]
   answer4.textContent = questions[0].choices[3]
   
-  answer1.addEventListener("click", question2)
+  answer1.addEventListener("click", incorrect)
   answer2.addEventListener("click", question2)
-  answer3.addEventListener("click", question2)
-  answer4.addEventListener("click", question2)
+  answer3.addEventListener("click", incorrect)
+  answer4.addEventListener("click", incorrect)
   
-  var selectedButton = e.target
-  
-  if (selectedButton === answer2){
-    alert("woohoo")
-  }
-
-  }
-
-
+}
 
 function question2() {
   quizH1.textContent = questions[1].question
@@ -84,9 +88,9 @@ function question2() {
   answer4.textContent = questions[1].choices[3]
 
   answer1.addEventListener("click", question3)
-  answer2.addEventListener("click", question3)
-  answer3.addEventListener("click", question3)
-  answer4.addEventListener("click", question3)
+  answer2.addEventListener("click", incorrect)
+  answer3.addEventListener("click", incorrect)
+  answer4.addEventListener("click", incorrect)
 }
 
 function question3() {
@@ -96,9 +100,9 @@ function question3() {
   answer3.textContent = questions[2].choices[2]
   answer4.textContent = questions[2].choices[3]
 
-  answer1.addEventListener("click", question4)
-  answer2.addEventListener("click", question4)
-  answer3.addEventListener("click", question4)
+  answer1.addEventListener("click", incorrect)
+  answer2.addEventListener("click", incorrect)
+  answer3.addEventListener("click", incorrect)
   answer4.addEventListener("click", question4)
 }
 
@@ -109,10 +113,10 @@ function question4() {
   answer3.textContent = questions[3].choices[2]
   answer4.textContent = questions[3].choices[3]
 
-  answer1.addEventListener("click", question5)
-  answer2.addEventListener("click", question5)
+  answer1.addEventListener("click", incorrect)
+  answer2.addEventListener("click", incorrect)
   answer3.addEventListener("click", question5)
-  answer4.addEventListener("click", question5)
+  answer4.addEventListener("click", incorrect)
 }
 
 function question5 () {
@@ -122,10 +126,10 @@ function question5 () {
   answer3.textContent = questions[4].choices[2]
   answer4.textContent = questions[4].choices[3]
 
-  answer1.addEventListener("click", question6)
+  answer1.addEventListener("click", incorrect)
   answer2.addEventListener("click", question6)
-  answer3.addEventListener("click", question6)
-  answer4.addEventListener("click", question6)
+  answer3.addEventListener("click", incorrect)
+  answer4.addEventListener("click", incorrect)
 }
 
 function question6 () {
@@ -135,29 +139,21 @@ function question6 () {
   answer3.textContent = questions[5].choices[2]
   answer4.textContent = questions[5].choices[3]
 
-  answer1.addEventListener("click", question7)
-  answer2.addEventListener("click", question7)
-  answer3.addEventListener("click", question7)
-  answer4.addEventListener("click", question7)
+  answer1.addEventListener("click", incorrect)
+  answer2.addEventListener("click", incorrect)
+  answer3.addEventListener("click", incorrect)
+  answer4.addEventListener("click", quizOver)
+
 }
 
-function question7 () {
-  quizH1.textContent = questions[6].question
-  answer1.textContent = questions[6].choices[0]
-  answer2.textContent = questions[6].choices[1]
-  answer3.textContent = questions[6].choices[2]
-  answer4.textContent = questions[6].choices[3]
-
-  answer1.addEventListener("click", quizDone)
-  answer2.addEventListener("click", quizDone)
-  answer3.addEventListener("click", quizDone)
-  answer4.addEventListener("click", quizDone)
+function quizOver() {
+  var score = counter
+  quizH1.textContent = "Your final Score is " + score
 }
-
 
 
 createBtn.addEventListener("click", quizStart);
-createBtn.addEventListener("click", setTime)
+createBtn.addEventListener("click", setTime);
 
 //need to determine if question was answered right or wrong
 //if right, display "correct"
